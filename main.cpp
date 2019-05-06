6674448605f8c4d96b9bd0a4b0536363650b7422
Simone Meloni <simone.meloni@cern.ch>
2019-05-06 12:00:49 +0200

    Change the main slightly to test some features

--- main.cpp
+++ main.cpp
@@ -7,15 +7,20 @@ int main(int, char**) {
     
     //If you want to parallelize the job execution you can do it here
     ToyExperiment* experiment = new ToyExperiment();
+    ToyExperiment* experiment2 = new ToyExperiment()
     experiment->setNRepetitions(10);
     experiment->setNData(1000.);
     experiment->setNMC(10000.);
     experiment->setOutputFileName("ResultsFile_0.root");
     
     ToyModule* module = new ToyModule();
+    ToyModule* module1 = new ToyModule();
 
     experiment->addModule(module);
+    experiment2->addModule(module);
+    experiment->addModule(module1);
     experiment->run();
+    experiment2->run();
 
     return 0;
 }
