17849f73dee99f66a0f80facf083832e10c41127
Simone Meloni <simone.meloni@cern.ch>
2019-05-06 17:15:20 +0200

    Add example of a class Parameters, inheriting from the FitParameters gneral class

---
+++ Examples/include/ExampleParameters.hpp
@@ -0,0 +1,13 @@
+#ifndef EXAMPLEPARAMETERS_HH
+#define EXAMPLEPARAMETERS_HH
+
+#include "FitParameters.hpp"
+
+class ExampleParameters : public FitParameters{
+
+public:
+    ExampleParameters();
+    ~ExampleParameters();
+};
+
+#endif
