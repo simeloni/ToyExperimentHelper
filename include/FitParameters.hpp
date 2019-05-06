2fc65d9efdb4b8d262d60d11617401a0155c149b
Simone Meloni <simone.meloni@cern.ch>
2019-05-06 14:46:59 +0200

    addParameter() and is_blind() functions in FitParameters

--- include/FitParameters.hpp
+++ include/FitParameters.hpp
@@ -10,6 +10,14 @@ public:
     FitParameters();
     ~FitParameters();
 
+    bool is_blind(); //return true if at least one of the parameters is blind
+
+protected:
+    //This method is protected since I want the specializations of these classed
+    // to use it only internally!
+    void addParameter(parameter* param);
+
+
 private: 
     std::vector<parameter*> _parameters;
 };
--- src/FitParameters.cpp
+++ src/FitParameters.cpp
@@ -1,4 +1,5 @@
 #include "FitParameters.hpp"
+#include <iostream>
 
 FitParameters::FitParameters() {
 
@@ -6,4 +7,24 @@ FitParameters::FitParameters() {
 
 FitParameters::~FitParameters() {
 
+}
+
+void FitParameters::addParameter(parameter* param) {
+    if (param == NULL) {
+        std::cout << "ERROR: in addParameter, FitParameters class. The pointer is pointing to NULL. Not adding any parameter " << std::endl;
+    }
+
+    else {
+        param->set_index(_parameters.size());
+        _parameters.push_back(param);
+    }
+}
+
+bool FitParameters::is_blind() {
+
+    for (unsigned int i = 0; i<_parameters.size(); i++) {
+        if (_parameters.at(i)->is_blind()) return true;
+    }
+    
+    return false;
 }
